3cf74b6adb0510ee7ae9c06ed0e8ade3feeb00fa
Tyler White <tylerwhitedesign@gmail.com>
2020-03-11 10:51:04 -0600

    Initial Commit

---
+++ MetalPinch/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2020 TylerWhiteDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
